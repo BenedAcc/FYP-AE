60f439d8eaee74f6f7042690200a0a71a72574ae
BenedAcc <161496903+BenedAcc@users.noreply.github.com>
2025-09-01 17:07:43 +0700

    ASG 2 FYP
    Tugas ASG 2 Ben 2902694130

---
+++ tugas_ben.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    char nama[100];
+    char Bahasa_favorit[100];
+    printf("Masukkan nama: ");
+    scanf("%s", nama);
+    printf("Bahasa pemrograman favorit: ");
+    scanf("%s", Bahasa_favorit);
+    printf("Hallo nama saya %s\n", nama);
+    printf("Bahasa pemrograman favorit saya %s\n", Bahasa_favorit);
+    return 0;
+
+}
